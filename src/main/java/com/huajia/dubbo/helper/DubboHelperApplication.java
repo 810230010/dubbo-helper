e1018fc6bc9d3a3bf100282bd3f46db0c5db9830
huajia <jiangjianping@souche.com>
2020-12-03 14:17:21 +0800

    add: :tada:初始化框架。

---
+++ src/main/java/com/huajia/dubbo/helper/DubboHelperApplication.java
@@ -0,0 +1,13 @@
+package com.huajia.dubbo.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huajia/dubbo/helper/DubboHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huajia.dubbo.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
